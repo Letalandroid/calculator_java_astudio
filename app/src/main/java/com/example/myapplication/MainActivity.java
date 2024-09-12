0fa067ba84243d78fd06edaed306f5947993923b
UCV-PIURA <UCV-PIURA@ucvvirtual.edu.pe>
2024-09-12 10:14:02 -0500

    Init with calculator

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_calcular;
+    EditText txtNum1;
+    EditText txtNum2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNum1 = findViewById(R.id.txtNum1);
+        txtNum2 = findViewById(R.id.txtNum2);
+        btn_calcular = findViewById(R.id.button);
+
+        btn_calcular.setOnClickListener(v -> {
+            Toast.makeText(v.getContext(),
+                    "El producto es: " + calcular(),
+                    Toast.LENGTH_LONG).show();
+        });
+    }
+
+    private int calcular() {
+
+        int num1 = Integer.parseInt(txtNum1.getText().toString());
+        int num2 = Integer.parseInt(txtNum2.getText().toString());
+
+        return num1 * num2;
+    }
+}
