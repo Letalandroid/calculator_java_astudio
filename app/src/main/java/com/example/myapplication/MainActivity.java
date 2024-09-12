b5a6167f2c0bd34863396a097a3376c688b4ed15
UCV-PIURA <UCV-PIURA@ucvvirtual.edu.pe>
2024-09-12 11:02:10 -0500

    Documentacion de codigo

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,39 +11,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Inicializacion de variables
     Button btn_calcular;
     EditText txtNum1;
     EditText txtNum2;
     TextView txtResult;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Creacion de la activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Llamada al diseño del activity
 
+        // Asignacion de valores de las variables antes llamadas
         txtNum1 = findViewById(R.id.txtNum1);
         txtNum2 = findViewById(R.id.txtNum2);
         txtResult = findViewById(R.id.txtResult);
         btn_calcular = findViewById(R.id.button);
-        txtResult.setText("");
+        txtResult.setText(""); // Iniciar el campo del resultado en vacio
 
-        btn_calcular.setOnClickListener(v -> {
-            txtResult.setText(setResult());
+        btn_calcular.setOnClickListener(v -> { // Funcion a realizar al momento que el usuario hace click
+            txtResult.setText(setResult()); // Recoje el valor obtenido en setResult() y lo muestra en txtResult
             Toast.makeText(v.getContext(),
                     setResult(),
-                    Toast.LENGTH_LONG).show();
+                    Toast.LENGTH_LONG).show(); // Muestra un pequeño Toast con el resultado de setResult
         });
     }
 
-    private float calcular() {
+    private float calcular() { // Funcion donde se realiza el proceso de calculo
 
+        // Obtener los valores de los EditText y convertirlos en flotante, reales o decimales
         float num1 = Float.parseFloat(txtNum1.getText().toString());
         float num2 = Float.parseFloat(txtNum2.getText().toString());
 
-        return num1 * num2;
+        return num1 * num2; // Operacion a devolver, una multiplicacion
     }
 
     private String setResult() {
-        return "El producto es: " + calcular();
+        return "El producto es: " + calcular(); // Mensaje a mostrar al terminar el proceso de calculo
     }
 }
